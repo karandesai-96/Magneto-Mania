1af7be20f63c33342b6b80393131b68447776e71
karandesai-96 <karandesai_96@live.com>
2016-04-07 17:23:12 +0530

    Declare screen size params in Main.java

--- android/src/com/sdsmdg/kd/magnetomania/android/AndroidLauncher.java
+++ android/src/com/sdsmdg/kd/magnetomania/android/AndroidLauncher.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.sdsmdg.kd.magnetomania.MagnetoManiaMain;
+import com.sdsmdg.kd.magnetomania.Main;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new MagnetoManiaMain(), config);
+		initialize(new Main(), config);
 	}
 }
--- core/src/com/sdsmdg/kd/magnetomania/Main.java
+++ core/src/com/sdsmdg/kd/magnetomania/Main.java
@@ -8,11 +8,16 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.sdsmdg.kd.screens.GameScreen;
 
 
-public class MagnetoManiaMain extends Game {
+public class Main extends Game {
+
+    public static float screenWidth;
+    public static float screenHeight;
 
 	@Override
 	public void create () {
         Gdx.app.log("MagnetoManiaMain", "create");
         setScreen(new GameScreen());
+        screenWidth = Gdx.graphics.getWidth();
+        screenHeight = Gdx.graphics.getHeight();
     }
 }
