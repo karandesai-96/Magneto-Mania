828328f1c45c35ed004e9a8d9d3ec4fe3ac20c2f
Karan Desai <karandesai_96@live.com>
2016-05-19 11:35:28 +0530

    Resolve indefinite GameState in HeatWaveController.

--- core/src/com/sdsmdg/kd/gameplay/controllers/HeatWaveController.java
+++ core/src/com/sdsmdg/kd/gameplay/controllers/HeatWaveController.java
@@ -2,6 +2,7 @@ package com.sdsmdg.kd.gameplay.controllers;
 
 import com.sdsmdg.kd.gameplay.objects.HeatWave;
 import com.sdsmdg.kd.gameplay.objects.Magnus;
+import com.sdsmdg.kd.gameworld.GameWorld;
 import com.sdsmdg.kd.magnetomania.Main;
 
 /**
@@ -15,11 +16,13 @@ public class HeatWaveController {
     }
 
     public void control(Magnus magnus) {
-        if (heatWave.active && heatWave.radius >= 3 * Main.screen.y / 2) {
-           heatWave.reset();
-        }
-        else {
-            heatWave.expand();
+        if (heatWave.active) {
+            if (heatWave.radius <= 3 * Main.screen.y / 2) {
+                heatWave.expand();
+            } else {
+                heatWave.reset();
+                GameWorld.gameState = GameWorld.GameState.NEXT_MAGNUS;
+            }
         }
     }
 }
