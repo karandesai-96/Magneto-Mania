9c3d12a39277b0b65a69ff72997b2b70bab56c4d
hareshkh <khanna.haresh@yahoo.co.in>
2016-05-19 19:21:38 +0530

    Initialises the endPoints of the Laser line segments

--- core/src/com/sdsmdg/kd/gameplay/objects/Laser.java
+++ core/src/com/sdsmdg/kd/gameplay/objects/Laser.java
@@ -9,9 +9,29 @@ import com.sdsmdg.kd.magnetomania.Main;
  */
 public class Laser extends GameObject {
     private RandomXS128 random;
+    public int activeTime;
+
+    /**
+     * This array of Vector2 type will store the end points of the line segment
+     * joining the center of the Magnus to the other end of the laser line. Thus
+     * the Laser obstacle basically consists of four line segments whose one end
+     * point is common and is at the center of the circle and other end point is
+     * defined by the following points.
+     */
+    public Vector2[] endPoints;
 
     public Laser() {
         this.random = new RandomXS128();
+        this.endPoints = new Vector2[5];
+        this.activeTime = 0;
+
+        //the weapon is Initially deactivated.
+        this.active = false;
+        endPoints[0] = new Vector2(-10,-10);
+        endPoints[1] = new Vector2(-10,-10);
+        endPoints[2] = new Vector2(-10,-10);
+        endPoints[3] = new Vector2(-10,-10);
+        endPoints[4] = new Vector2(-10,-10);
     }
 
     public void setMagnusVelocityComponents (Magnus magnus) {
