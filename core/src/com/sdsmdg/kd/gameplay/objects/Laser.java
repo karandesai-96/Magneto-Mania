83d0ce1c37312ac0336e3cc5118628d0931c061f
hareshkh <khanna.haresh@yahoo.co.in>
2016-05-20 15:35:53 +0530

    Adds necessary variables and `reset` method

--- core/src/com/sdsmdg/kd/gameplay/objects/Laser.java
+++ core/src/com/sdsmdg/kd/gameplay/objects/Laser.java
@@ -2,6 +2,7 @@ package com.sdsmdg.kd.gameplay.objects;
 
 import com.badlogic.gdx.math.RandomXS128;
 import com.badlogic.gdx.math.Vector2;
+import com.sdsmdg.kd.helpers.InputHandler;
 import com.sdsmdg.kd.magnetomania.Main;
 
 /**
@@ -10,7 +11,25 @@ import com.sdsmdg.kd.magnetomania.Main;
 public class Laser extends GameObject {
     private RandomXS128 random;
     public Vector2 screenCenter;
-    public int activeTime;
+
+    /**
+     * Denotes the number of quarter turns.
+     */
+    public int numberOfTurns;
+
+    /**
+     * Denotes the time taken for on quarter turn.
+     */
+
+    public int turnTime;
+
+    /**
+     * There is a need for two point velocities because each couple of diametrically
+     * opposite points have different velocities as they have different distances to
+     * cover in the same amount of time, the two different distances being: Height and
+     * Width.
+     */
+    public int[] pointVelocity;
 
     /**
      * This array of Vector2 type will store the end points of the line segment
@@ -24,8 +43,9 @@ public class Laser extends GameObject {
     public Laser() {
         this.random = new RandomXS128();
         this.endPoints = new Vector2[5];
-        this.activeTime = 0;
-
+        this.pointVelocity = new int[2];
+        this.numberOfTurns = 0;
+        this.turnTime = 0;
         this.screenCenter = new Vector2((Main.screen.x)/2,(Main.screen.y)/2);
 
         //the weapon is Initially deactivated.
@@ -42,6 +62,8 @@ public class Laser extends GameObject {
         endPoints[3].y = -10;
         endPoints[4].x = -10;
         endPoints[4].y = -10;
+        pointVelocity[0] = 0;
+        pointVelocity[1] = 0;
     }
 
     public void setMagnusVelocityComponents (Magnus magnus) {
@@ -49,14 +71,18 @@ public class Laser extends GameObject {
         magnus.calcVelocityComponent(screenCenter);
     }
 
-    public void moveMagnusToCentre(Magnus magnus) {
+    public void moveMagnus(Magnus magnus) {
         magnus.add(velocityComponent);
     }
 
     public void init (Magnus magnus) {
+        activate();
         setMagnusVelocityComponents(magnus);
-        moveMagnusToCentre(magnus);
-
+        moveMagnus(magnus);
+        numberOfTurns = 4 + random.nextInt(12);
+        turnTime = 20 + random.nextInt(50);
+        pointVelocity[0] = (int) (Main.screen.x/turnTime); // lesser speed
+        pointVelocity[1] = (int) (Main.screen.y/turnTime); // greater speed
         endPoints[0] = screenCenter;
         endPoints[1].x = 0;
         endPoints[1].y = 0;
@@ -66,5 +92,20 @@ public class Laser extends GameObject {
         endPoints[3].y = Main.screen.y;
         endPoints[4].x = Main.screen.x;
         endPoints[4].y = 0;
+    }
+
+    public void reset (Magnus magnus) {
+        deactivate();
+        magnus.calcVelocityComponent(new Vector2(InputHandler.touch.x,InputHandler.touch.y));
+        moveMagnus(magnus);
+        numberOfTurns = 0;
+        turnTime = 0;
+        pointVelocity[0] = pointVelocity[1] = 0;
+        endPoints[0].x = endPoints[1].x = endPoints[2].x = endPoints[3].x = endPoints[4].x = -10;
+        endPoints[0].y = endPoints[1].y = endPoints[2].y = endPoints[3].y = endPoints[4].y = -10;
+    }
+
+    public void rotateLaser () {
+        
     }
 }
