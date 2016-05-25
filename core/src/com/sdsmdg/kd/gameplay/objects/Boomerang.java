b3a763d77b820ae5e2f0d31890e6712cb1c8a731
Karan Desai <karandesai_96@live.com>
2016-05-25 17:27:14 +0530

    Remove accelerationComponent implementation from boomerang.

--- core/src/com/sdsmdg/kd/gameplay/objects/Boomerang.java
+++ core/src/com/sdsmdg/kd/gameplay/objects/Boomerang.java
@@ -9,11 +9,11 @@ import com.sdsmdg.kd.magnetomania.Main;
 
 /**
  * @author Haresh Khanna
+ * @author Karan Desai
  */
 public class Boomerang extends GameObject{
     public float acceleration;
-    public Vector2 accelerationComponent;
-    private Vector2 inititalTouch;
+    private Vector2 initialTouch;
     private RandomXS128 random;
 
     public Boomerang () {
@@ -31,36 +31,24 @@ public class Boomerang extends GameObject{
         // Boomerang is inactive when game starts.
         this.active = false;
 
-        this.accelerationComponent = new Vector2(0,0);
-        this.inititalTouch = new Vector2(0,0);
+        this.initialTouch = new Vector2(0,0);
         this.random = new RandomXS128();
     }
 
-    public void calcAccelerationComponent (Vector2 source ,Vector2 destination) {
-        float distance = source.dst(destination);
-
-        // acceleration times cos(theta)
-        this.accelerationComponent.x = acceleration * (destination.x - source.x) / distance;
-
-        // acceleration times sin(theta)
-        this.accelerationComponent.y = acceleration * (destination.y - source.y) / distance;
-    }
 
     public void init (Magnus magnus) {
         activate();
         this.x = magnus.x;
         this.y = magnus.y;
 
-        this.velocity = 50 + random.nextInt(30);
-        this.acceleration = (float)0.25 + random.nextFloat();
+        this.velocity = 30 + random.nextInt(15);
+        this.acceleration = 0.25f + random.nextFloat() / 5;
 
-        this.inititalTouch.set(InputHandler.touch.x,InputHandler.touch.y);
+        this.initialTouch.set(InputHandler.touch.x, InputHandler.touch.y);
 
-        calcVelocityComponent(inititalTouch);
-        calcAccelerationComponent(new Vector2(magnus.x,magnus.y),inititalTouch);
+        calcVelocityComponent(initialTouch);
         Gdx.app.log("Boomerang","Velocity and Acceleration set");
-        Gdx.app.log("Boomerang","Velocity components : "+velocityComponent.x+" "+velocityComponent.y);
-        Gdx.app.log("Boomerang","Acceleration components : "+accelerationComponent.x+" "+accelerationComponent.y);
+        Gdx.app.log("Boomerang","Velocity components : " + velocityComponent.x + " " + velocityComponent.y);
     }
 
     public void reset () {
@@ -70,16 +58,12 @@ public class Boomerang extends GameObject{
 
         this.velocity = 0;
         this.acceleration = 0;
-
-        this.accelerationComponent.set(0,0);
-        this.inititalTouch.set(0,0);
     }
 
     public void shootBoomerang (Magnus magnus) {
         add(velocityComponent);
-        velocityComponent.x = velocityComponent.x - accelerationComponent.x;
-        velocityComponent.y = velocityComponent.y - accelerationComponent.y;
-        //calcVelocityComponent(new Vector2(magnus.x,magnus.y),inititalTouch);
-        Gdx.app.log("Boomerang","Velocity components : "+velocityComponent.x+" "+velocityComponent.y);
+        velocity -= acceleration;
+        calcVelocityComponent(new Vector2(magnus.x, magnus.y), initialTouch);
+        Gdx.app.log("Boomerang", "Velocity components : " + velocityComponent.x + " " + velocityComponent.y);
     }
 }
