52bbd68d876446a9cee581b55a885414bea0288d
Karan Desai <karandesai_96@live.com>
2016-06-21 21:23:12 +0530

    Refactor members and init method of laser.

--- core/src/com/sdsmdg/kd/gameplay/objects/Laser.java
+++ core/src/com/sdsmdg/kd/gameplay/objects/Laser.java
@@ -1,6 +1,5 @@
 package com.sdsmdg.kd.gameplay.objects;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.RandomXS128;
 import com.badlogic.gdx.math.Vector2;
 import com.sdsmdg.kd.helpers.InputHandler;
@@ -8,56 +7,20 @@ import com.sdsmdg.kd.magnetomania.Main;
 
 /**
  * @author Haresh Khanna
+ * @author Karan Desai
  */
 public class Laser extends GameObject {
     private RandomXS128 random;
-
-    /**
-     * Denotes the number of quarter turns.
-     */
-    public int numberOfTurns;
-
-    /**
-     * Denotes the time taken for on quarter turn.
-     */
-    public int turnTime;
-
-    /**
-     * There is a need for two point velocities because each couple of diametrically
-     * opposite points have different velocities as they have different distances to
-     * cover in the same amount of time, the two different distances being: Height and
-     * Width.
-     */
-    public int[] pointVelocity;
-
-    /**
-     * This array of Vector2 type will store the end points of the line segment
-     * joining the center of the Magnus to the other end of the laser line. Thus
-     * the Laser obstacle basically consists of four line segments whose one end
-     * point is common and is at the center of the circle and other end point is
-     * defined by the following points.
-     */
-    public Vector2[] endPoints;
+    public final Vector2 center = new Vector2(Main.screen.x, Main.screen.y);
 
     public Laser() {
         this.random = new RandomXS128();
 
-        /**
-         * For instantiating a Vector2 type array.
-         */
-        this.endPoints = new Vector2[5];
-        for (int i=0;i<5;i++) {
-            this.endPoints[i] = new Vector2(-10,-10);
-        }
-        this.pointVelocity = new int[2];
-        this.numberOfTurns = 0;
-        this.turnTime = 0;
+        this.x = Main.screen.x;
+        this.y = Main.screen.y;
 
         //the weapon is Initially deactivated.
         this.active = false;
-
-        //sets the end points out of the screen initially
-        pointVelocity[0] = pointVelocity[1] = 0;
     }
 
     public void fixMagnusPathToFinger (Magnus magnus) {
@@ -73,60 +36,55 @@ public class Laser extends GameObject {
         magnus.add(magnus.velocityComponent);
     }
 
-    public void init (Magnus magnus) {
-        Gdx.app.log("Laser","Initialised");
+    /**
+     * Set the end point to one of the corners of screen, to start
+     * sweeping in anticlockwise direction.
+     * @param orientation To decide the corner for end point.
+     */
+    public void init(int orientation) {
         activate();
-        moveMagnusToCenter(magnus);
-        numberOfTurns = 4 + random.nextInt(12);
-        turnTime = 30 + random.nextInt(50);
-        pointVelocity[0] = (int) (Main.screen.x/turnTime); // lesser speed
-        pointVelocity[1] = (int) (Main.screen.y/turnTime); // greater speed
 
-        // center point
-        endPoints[0].set(Main.screenCenter.x,Main.screenCenter.y);
-
-        //left up
-        endPoints[1].set(0,0);
-
-        //left down
-        endPoints[2].set(0,Main.screen.y);
-
-        //right down
-        endPoints[3].set(Main.screen.x,Main.screen.y);
-
-        //right up
-        endPoints[4].set(Main.screen.x,0);
-
-        magnus.velocity = random.nextInt(15) + 10;
-    }
-
-    public void reset (Magnus magnus) {
-        deactivate();
-        fixMagnusPathToFinger(magnus);
-        moveMagnusToBoundary(magnus);
-        numberOfTurns = 0;
-        turnTime = 0;
-        pointVelocity[0] = pointVelocity[1] = 0;
-        endPoints[0].x = endPoints[1].x = endPoints[2].x = endPoints[3].x = endPoints[4].x = -10;
-        endPoints[0].y = endPoints[1].y = endPoints[2].y = endPoints[3].y = endPoints[4].y = -10;
-        magnus.velocity = 0;
-    }
-
-    public void rotateLaser () {
-        if (endPoints[1].y < Main.screen.y || endPoints[2].x < Main.screen.x
-                || endPoints[3].y > 0 || endPoints[4].x>0) {
-            endPoints[2].x = endPoints[2].x + pointVelocity[0];
-            endPoints[4].x = endPoints[4].x - pointVelocity[0];
-            endPoints[1].y = endPoints[1].y + pointVelocity[1];
-            endPoints[3].y = endPoints[3].y - pointVelocity[1];
+        if (orientation == 0) {
+            this.set(2 * center.x, 0);    // Top Right
         }
-        else {
-            numberOfTurns --;
-            Vector2 temp = endPoints[1];
-            endPoints[1] = endPoints[4];
-            endPoints[4] = endPoints[3];
-            endPoints[3] = endPoints[2];
-            endPoints[2] = temp;
+        else if (orientation == 1) {
+            this.set(0, 0);    // Top Left
+        }
+        else if (orientation == 2) {
+            this.set(0, 2 * center.y);
+        }
+        else if (orientation == 3) {
+            this.set(2 * center.x, 2 * center.y);
         }
     }
+
+//    public void reset (Magnus magnus) {
+//        deactivate();
+//        fixMagnusPathToFinger(magnus);
+//        moveMagnusToBoundary(magnus);
+//        numberOfTurns = 0;
+//        turnTime = 0;
+//        pointVelocity[0] = pointVelocity[1] = 0;
+//        endPoints[0].x = endPoints[1].x = endPoints[2].x = endPoints[3].x = endPoints[4].x = -10;
+//        endPoints[0].y = endPoints[1].y = endPoints[2].y = endPoints[3].y = endPoints[4].y = -10;
+//        magnus.velocity = 0;
+//    }
+//
+//    public void rotateLaser () {
+//        if (endPoints[1].y < Main.screen.y || endPoints[2].x < Main.screen.x
+//                || endPoints[3].y > 0 || endPoints[4].x>0) {
+//            endPoints[2].x = endPoints[2].x + pointVelocity[0];
+//            endPoints[4].x = endPoints[4].x - pointVelocity[0];
+//            endPoints[1].y = endPoints[1].y + pointVelocity[1];
+//            endPoints[3].y = endPoints[3].y - pointVelocity[1];
+//        }
+//        else {
+//            numberOfTurns --;
+//            Vector2 temp = endPoints[1];
+//            endPoints[1] = endPoints[4];
+//            endPoints[4] = endPoints[3];
+//            endPoints[3] = endPoints[2];
+//            endPoints[2] = temp;
+//        }
+//    }
 }
