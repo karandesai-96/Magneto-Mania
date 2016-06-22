0dd0cd893312dc436b81bbd40d6f7405aaef3abf
Karan Desai <karandesai_96@live.com>
2016-06-22 14:23:55 +0530

    Add four laser arms in laser class.

--- core/src/com/sdsmdg/kd/gameplay/objects/Laser.java
+++ core/src/com/sdsmdg/kd/gameplay/objects/Laser.java
@@ -1,6 +1,5 @@
 package com.sdsmdg.kd.gameplay.objects;
 
-import com.badlogic.gdx.math.RandomXS128;
 import com.badlogic.gdx.math.Vector2;
 import com.sdsmdg.kd.helpers.InputHandler;
 import com.sdsmdg.kd.magnetomania.Main;
@@ -10,15 +9,14 @@ import com.sdsmdg.kd.magnetomania.Main;
  * @author Karan Desai
  */
 public class Laser extends GameObject {
-    private RandomXS128 random;
     public final Vector2 center = new Vector2(Main.screen.x, Main.screen.y);
+    public Vector2[] endPoints = new Vector2[4];
+
 
     public Laser() {
-        this.random = new RandomXS128();
-
-        this.x = Main.screen.x;
-        this.y = Main.screen.y;
-
+        for (int i = 0; i < 4; i++) {
+            this.endPoints[i].set(Main.screen);
+        }
         //the weapon is Initially deactivated.
         this.active = false;
     }
@@ -39,41 +37,23 @@ public class Laser extends GameObject {
     /**
      * Set the end point to one of the corners of screen, to start
      * sweeping in anticlockwise direction.
-     * @param orientation To decide the corner for end point.
      */
-    public void init(int orientation) {
+    public void init() {
         activate();
-
-        if (orientation == 0) {
-            this.set(2 * center.x, 0);    // Top Right
-        }
-        else if (orientation == 1) {
-            this.set(0, 0);    // Top Left
-        }
-        else if (orientation == 2) {
-            this.set(0, 2 * center.y);
-        }
-        else if (orientation == 3) {
-            this.set(2 * center.x, 2 * center.y);
-        }
+        this.endPoints[0].set(2 * center.x, 0);            // Top Right
+        this.endPoints[1].set(0, 0);                       // Top Left
+        this.endPoints[2].set(0, 2 * center.y);            // Bottom Left
+        this.endPoints[3].set(2 * center.x, 2 * center.y); // Bottom Right
     }
 
-    public void rotate(int orientation) {
-        if (orientation == 0) {
-            this.x -= Main.screen.x / 30.0;
-        }
-        else if (orientation == 1) {
-            this.y += Main.screen.y / 30.0;
-        }
-        else if (orientation == 2) {
-            this.x += Main.screen.x / 30.0;
-        }
-        else if (orientation == 3) {
-            this.y -= Main.screen.y / 30.0;
-        }
+    public void rotate() {
+        this.endPoints[0].x -= Main.screen.x / 30.0;
+        this.endPoints[1].y += Main.screen.y / 30.0;
+        this.endPoints[2].x += Main.screen.x / 30.0;
+        this.endPoints[3].y -= Main.screen.y / 30.0;
     }
 
-    public void reset (Magnus magnus) {
+    public void reset () {
         deactivate();
     }
 }
