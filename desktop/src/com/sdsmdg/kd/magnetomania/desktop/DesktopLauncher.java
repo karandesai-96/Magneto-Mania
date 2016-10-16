6ee159c7c3a3e1fc217b83169817cb10385de5d0
Haresh Khanna <khanna.haresh@yahoo.co.in>
2016-10-16 23:06:08 +0530

    Minor Fixes

--- desktop/src/com/sdsmdg/kd/magnetomania/desktop/DesktopLauncher.java
+++ desktop/src/com/sdsmdg/kd/magnetomania/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.sdsmdg.kd.magnetomania.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.sdsmdg.kd.magnetomania.MagnetoManiaMain;
+import com.sdsmdg.kd.magnetomania.Main;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MagnetoManiaMain(), config);
+		new LwjglApplication(new Main(), config);
 	}
 }
